6535990d11d7d0e53ada6b9b503bfdf08d889710
User <TejaMac@userdemacbook-pro.local>
2017-10-18 16:14:17 +0530

    Initial Commit

---
+++ BitcoinSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinSample
+//
+//  Created by Teja's MacBook on 26/07/1939 Saka.
+//  Copyright © 1939 Saka Vedas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
